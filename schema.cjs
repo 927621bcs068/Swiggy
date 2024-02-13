f7b1853250a58740bbf35ec1f0d2180c977e44db
Mohanraj J <142493131+927621bcs068@users.noreply.github.com>
2024-02-13 15:56:51 +0530

    Add files via upload

---
+++ schema.cjs
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const RestaurantSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    res_info:
+    {   type:Number
+
+    },
+    cuisines:{
+        type:Array
+    },
+    area:{
+        type:String
+    }
+
+})
+
+//Creating model
+const Restaurants=mongoose.model('RestaurantSchema',RestaurantSchema)
+
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    contact:
+    {   type:Number
+
+    },
+    email:{
+        type:String
+    },
+    place:{
+        type:String
+    }
+
+})
+const Users=mongoose.model('Userlist',UserSchema)
+//exporting model
+module.exports={Users,Restaurants}
---
+++ server.cjs
@@ -0,0 +1,116 @@
+//Express is a dependency file whereas Nodemmon developer friendly file: npm install nodemon --save-de
+const bodyparser =require('body-parser')
+const cors=require('cors')
+const express =require('express')
+const mongoose =require('mongoose')
+
+
+const {Restaurants, Users}=require('./schema.cjs')
+
+const app =express()
+app.use(bodyparser.json())
+app.use(cors())
+// app.listen(8000, function() {
+//     console.log(Listening on port 8000)
+// })
+
+async function connectToDb() {
+    try {
+        await mongoose.connect('mongodb+srv://mohanraja:1045@cluster0.ciy6hgg.mongodb.net/?retryWrites=true&w=majority')
+
+        console.log('Connection Established ;)')
+        const port = process.env.Port || 8000
+        app.listen(port, function() {
+            console.log(`Listening on port ${port}...`)
+        })
+    } catch(error) {
+        console.log(error)
+        console.log('Couldn\'t Establish connection :(')
+    }
+}
+
+connectToDb()
+
+// /add_restaurant : post
+// /get-restaurant details :get
+// /create-new-user :post
+// /validate-user :post
+
+app.post('/add-restaurant',async (request,response)=>{
+    try{    await Restaurants.create({
+            "name":request.body.name,
+            "res_info":request.body.res_info,
+            "cuisines":request.body.cuisines,
+            "area":request.body.area
+            })
+            response.status(201).json({
+                "status":"success",
+                "message":"Restaurant added"
+            })
+       }
+    catch(error)
+    {   
+        console.log(error)
+        response.status(500).json({
+            "status":"Error occoured",
+            "message":error
+            
+        })
+    }     
+})
+
+//get-restaurants :it is  a api for getting all the list of restaurant fata from the database
+app.get('/get-restaurant-details',async function (request,response){
+    try{
+        const restaurantDetails =await Restaurants.find()
+        response.json(restaurantDetails)
+    }catch(error){ 
+
+        response.status(500).json({
+        "status":"Error occoured",
+        "message":"could not fetch",
+        "error":error
+        })
+    }
+})
+
+//Post details of Users
+app.post('/add-Users',async (request,response)=>{
+    try{    
+            await Users.create({
+            "name":request.body.name,
+            "contact":request.body.contact,
+            "email":request.body.email,
+            "place":request.body.place
+            })
+            console.log(request.body)
+            response.status(201).json({
+                "status":"success",
+                "message":"User created"
+            })
+       }
+    catch(error)
+    {   
+        console.log(error)
+        response.status(500).json({
+            "status":"Error occoured",
+            "message":error
+            
+        })
+    }     
+})
+
+//get-uers :it is  a api for getting all the list of users data from the database
+app.get('/get-user-details',async function (request,response){
+    try{
+        const UsersDetails =await Users.find()
+        response.json(UsersDetails)
+    }catch(error){ 
+
+        response.status(500).json({
+        "status":"Error occoured",
+        "message":"could not fetch",
+        "error":error
+        })
+    }
+})
